7e8e55c11c31268646bcef5549b827fce5ad2d8c
ramilmustafin <ramilmustafin33@gmail.com>
2026-01-25 15:09:01 +0400

    feat: Implement new fuzz tests for `withdraw` and `transfer` functionality, and adjust `RebaseToken`'s `PRECISION_FACTOR`.

--- src/RebaseToken.sol
+++ src/RebaseToken.sol
@@ -16,7 +16,7 @@ using SafeERC20 for ERC20;
 contract RebaseToken is ERC20, Ownable, AccessControl{
     error RebaseToken_InterestRateCanOnlyDecrease(uint256, uint256);
 
-    uint256 private constant PRECISION_FACTOR = 1e27;
+    uint256 private constant PRECISION_FACTOR = 1e18;
     bytes32 private constant MINT_AND_BURN_ROLE = keccak256("MINT_AND_BURN_ROLE");
     uint256 private s_interestRate = (5 * PRECISION_FACTOR)/1e8;
     mapping(address => uint256) private s_userInterestRate;
--- test/FuzzRebaseToken.t.sol
+++ test/FuzzRebaseToken.t.sol
@@ -18,8 +18,11 @@ contract RebaseTokenTest is Test {
         rebaseToken = new RebaseToken(owner);
         vault = new Vault(IRebaseToken(address(rebaseToken)));
         rebaseToken.grantMintAndBurnRole(address(vault));
-        payable(address(vault)).call{value: 1 ether}("");
         vm.stopPrank();
+    }
+
+    function addRewardsToVault(uint256 rewardAmount) public {
+        (bool success,) = payable(address(vault)).call{value:rewardAmount}("");
     }
 
     function testDeposit(uint256 amount) public {
@@ -42,7 +45,7 @@ contract RebaseTokenTest is Test {
         vm.stopPrank();
     }
 
-    function testRedeem(uint256 amount) public {
+    function testWithdraw(uint256 amount) public {
         amount = bound(amount, 1e5, type(uint96).max);
         vm.startPrank(user);
         vm.deal(user, amount);
@@ -53,4 +56,59 @@ contract RebaseTokenTest is Test {
         assertEq(address(user).balance, amount);
         vm.stopPrank();
     }
+
+    function testWithdrawAfterTimePassed(uint256 amount, uint256 time) public {
+        time = bound(time, 1000, type(uint32).max);
+        amount = bound(amount, 1e5, type(uint96).max);
+        vm.prank(user);
+        vm.deal(user, amount);
+        vault.deposit{value:amount}();
+
+        vm.warp(block.timestamp + time);
+        uint256 balance = rebaseToken.balanceOf(user);
+
+        vm.deal(owner, balance - amount);
+        vm.prank(owner);
+        addRewardsToVault(balance - amount);
+        vm.prank(user);
+        vault.withdraw(type(uint256).max);
+
+        uint256 ethBalance = address(user).balance;
+
+        assertEq(ethBalance, balance);
+        assertGt(ethBalance, amount);
+    }
+
+    function testTransfer(uint256 amount, uint256 amountToSend) public {
+        amount = bound(amount, 2e5, type(uint96).max);
+        amountToSend = bound(amountToSend, 1e5, amount - 1e5);
+
+        vm.deal(user, amount);
+        vm.prank(user);
+        vault.deposit{value:amount}();
+
+        address user2 = makeAddr("user2");
+        uint256 userBalance = rebaseToken.balanceOf(user);
+        uint256 user2Balance = rebaseToken.balanceOf(user2);
+        assertEq(userBalance, amount);
+        assertEq(user2Balance, 0);
+
+        vm.prank(owner);
+        rebaseToken.setInterestRate(4e10);
+
+        vm.prank(user);
+        rebaseToken.transfer(user2, amountToSend);
+
+        uint256 userBalanceAfterTransfer = rebaseToken.balanceOf(user);
+        uint256 user2BalanceAfterTransfer = rebaseToken.balanceOf(user2);
+    
+        assertEq(userBalanceAfterTransfer, amount - amountToSend);
+        assertEq(user2BalanceAfterTransfer, amountToSend);
+
+        // Check the user interest rate has been inherited
+        assertEq(rebaseToken.getUserInterestRate(user), 5e10);
+        assertEq(rebaseToken.getUserInterestRate(user2), 5e10);
+    }
+
+    
 }
